05f4d0efe76ab346a3ea064887d475455ecf155a
Natalia <natalia.kitesurf@gmail.com>
2022-10-18 00:03:23 +0100

    feat: remove-from-list

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -29,40 +29,29 @@ const { NotImplementedError } = require('../extensions/index.js');
 //   }
 // }
 function removeKFromList(l, k ) {
-  // for (let i=0; i<l.length; i++) {
-  //   l[i] = new ListNode(l[i]);
-  // };
-  // console.log(l);
-  // for (let i=0; i<l.length-1; i++) {
-  //   l[i].next = l[i+1];
-  // };
-  let headList = l[0];
-  for (let i=0; i<l.length; i++) {
-    if (l[i]/*.value*/ === k) {
-      if (i === 0) {
-       l.shift()
-      } else if (i === l.length - 1) {
-        l.pop();
-      } else {
-         l.splice(i,1);
-         } 
-        }
+  while (l.value === k) {
+    l = l.next;
+  };
+  let prev = l,
+  cur = prev.next,
+  fut = cur.next;
+  while (cur != null) {
+    if (cur.value === k) {
+      prev.next = fut;
+      cur = fut;
+      fut = cur.next;
+    } else {
+    prev = cur;
+    cur = prev.next;
+    fut = cur.next;
+    if (fut == null) {break};
     }
-    for (let i=0; i<l.length-1; i++) {
-      l[i].next = l[i+1];
-      };
-  
-  console.log(l);
-  // l = l.filter(elem => elem !== null);
-  console.log(headList);
-  // let values = [];
-  // for (let elem of l ) {values.push(elem.value)}
-  // console.log(values);
+  };
+  if (cur.value === k) {prev.next = null};
   return l;
+  };
   
-}
 
 module.exports = {
   removeKFromList
 };
-console.log(removeKFromList([3,1,2,3,4,5],3));
