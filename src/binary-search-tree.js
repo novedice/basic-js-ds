e11fa03fd1b58c2b8434533c13a8f253ae29bcc2
Natalia <natalia.kitesurf@gmail.com>
2022-10-17 13:29:17 +0100

    feat: stack

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -6,16 +6,41 @@ const { NotImplementedError } = require('../extensions/index.js');
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
+class Node {
+  constructor (val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
 class BinarySearchTree {
-
-  root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor () {
+    this.root = null;
+    // this.node = {
+    //   data: data,
+    //   left: null,
+    //   right: null
+    // }
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  // root() {
+  //   return this.root;
+  // }
+
+  add(data) {
+   this.root = addFunction (this.root, data);
+   function addFunction (node, data) {
+      if (!node) {return new Node (data)};
+      if (node.val === data) {
+        return node;
+      }
+      if (data > node.val) {
+        node.right = addFunction(node.right, data);
+      } else {
+        node.left = addFunction(node.left, data);
+      };
+      return node;
+   };
   }
 
   has(/* data */) {
@@ -47,3 +72,8 @@ class BinarySearchTree {
 module.exports = {
   BinarySearchTree
 };
+const tree = new BinarySearchTree();
+      tree.add(2);
+      tree.add(3);
+      console.log(tree.add(4));
+      // console.log (tree.root());
--- src/stack.js
+++ src/stack.js
@@ -13,22 +13,37 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.stackAr = [];
+    this.stackLength = 0;
+    this.stackHead = null;
+  }
+  push(element) {
+   this.stackAr.push(element);
+   this.stackLength +=1;
+   this.stackHead = element;
+   return this;
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.stackLength -= 1;
+    if (this.stackLength <0) {return undefined};
+    if (this.stackLength == 0) {
+      this.stackHead = null;
+      return this.stackAr.pop();
+    };
+    if (this.stackLength > 0) {
+      this.stackHead = this.stackAr[this.stackAr.length-2];
+      return this.stackAr.pop();
+    };
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+   return this.stackHead;
   }
 }
 
 module.exports = {
   Stack
 };
+
